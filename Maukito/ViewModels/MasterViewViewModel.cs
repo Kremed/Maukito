c9184df1a9c21a0890742a0e93f6a3d948e6a27e
Moatasem La Kremed <moatasemkremed@yahoo.com>
2023-02-20 03:46:13 +0200

    - MasterViewViewModel [ISSUE TO FIX] GetScreenSize() / screen size static Width & Hight in Android Platform.

--- Maukito/ViewModels/MasterViewViewModel.cs
+++ Maukito/ViewModels/MasterViewViewModel.cs
@@ -1,6 +1,4 @@
-﻿
-
-namespace Maukito.ViewModels;
+﻿namespace Maukito.ViewModels;
 
 public partial class MasterViewViewModel : BaseViewModel
 {
@@ -23,28 +21,43 @@ public partial class MasterViewViewModel : BaseViewModel
     {
         try
         {
-            DisplayHeight = Application.Current.MainPage.Height;
-
-            VidMargin = VidMargin - DisplayHeight;
-
-            var ScreenHight = Application.Current.MainPage.Height;
-
-            var HightToAdd = Application.Current.MainPage.Height / 5;
-
-            var ScreenWidth = Application.Current.MainPage.Width;
-
-            var WidthToAdd = Application.Current.MainPage.Width / 5;
-
-            //To Add About 20% for Zooming The Video Media element =>
-
-            VidHight = ScreenHight + HightToAdd;
-
-            VidWidth = ScreenWidth + HightToAdd;
+            GetScreenSize();
         }
         catch (Exception ex)
         {
             var msg = Snackbar.Make($"{ex.Message} - {ex.InnerException}", null, "X", TimeSpan.FromSeconds(10), BaseSnackbarOptions).Show();
         }
+    }
+
+    [RelayCommand]
+    public void GetScreenSize()
+    {
+        DisplayHeight = Application.Current.MainPage.Height;
+
+        VidMargin = VidMargin - DisplayHeight;
+
+        var ScreenHight = Application.Current.MainPage.Height;
+
+        var HightToAdd = Application.Current.MainPage.Height / 5;
+
+        var ScreenWidth = Application.Current.MainPage.Width;
+
+        var WidthToAdd = Application.Current.MainPage.Width / 5;
+
+        //To Add About 20% for Zooming The Video Media element =>
+
+#if ANDROID
+
+        //[ISSUE] TO Fix => For some reason, the Height and Width of the client's screen are not obtained in the
+        //Android application for the main page of the application. We always get a value of -1 for Width & Hight.
+        //This problem has been temporarily fixed by specifying a static Width & Hight in Android Platform.
+
+        VidHight = 900;
+        VidWidth = 600;
+#elif IOS
+        VidHight = ScreenHight + HightToAdd;
+        VidWidth = ScreenWidth + WidthToAdd;
+#endif
     }
 
     [RelayCommand]
--- Maukito/Views/MasterViewPage.xaml.cs
+++ Maukito/Views/MasterViewPage.xaml.cs
@@ -1,10 +1,17 @@
-﻿namespace Maukito.Views;
+﻿
+namespace Maukito.Views;
 
 public partial class MasterViewPage : ContentPage
 {
-	public MasterViewPage(MasterViewViewModel viewModel)
-	{
-		InitializeComponent();
-		BindingContext = viewModel;
-	}
+    public MasterViewPage(MasterViewViewModel viewModel)
+    {
+        InitializeComponent();
+
+        BindingContext = viewModel;
+    }
+
+    protected override void LayoutChildren(double x, double y, double width, double height)
+    {
+        base.LayoutChildren(x, y, width, height);
+    }
 }
